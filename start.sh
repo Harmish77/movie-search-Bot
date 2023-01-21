71173a29ef943c045cc653135366032494260c4b
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-01-22 00:50:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
+git clone https://github.com/Harmish77/movie-search-Bot/MdiskWalaBot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
