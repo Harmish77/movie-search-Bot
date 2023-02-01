ecd0a1c77c310cc6d6fa11547e6e326648492c6d
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-02-01 23:04:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Harmish77/movie-search-Bot /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Harmish77/movie-search-Bot /movie-search-Bot
+cd /movie-search-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
