ccb1a40d3892bd95c6e9c8fd4c3f580aca7cf329
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-02-16 21:29:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ async def message_handler(event):
             author=Config.BOT_USERNAME
         )
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
-newbutton = [Button.url('Join',f'http://t.me/main_movie_hub')]
+        
         await txt.delete()
         result = await event.reply(message, link_preview=False)
         await asyncio.sleep(Config.AUTO_DELETE_TIME)
